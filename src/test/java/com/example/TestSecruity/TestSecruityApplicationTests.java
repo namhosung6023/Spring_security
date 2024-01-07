60aa5c261dcafbeb88e3e6760b46fd9f9fb0088c
namhosung6023 <hosung60023@naver.com>
2024-01-07 19:27:41 +0900

    start

---
+++ src/test/java/com/example/TestSecruity/TestSecruityApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.TestSecruity;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestSecruityApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
